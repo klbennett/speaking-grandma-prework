7845f55d810a2c49216309fb78f1a8642b2b1ce7
Kay B <kaylouisebennett@gmail.com>
2018-08-11 09:06:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(string)
   elsif
     string != "string".upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif
+  else
     string == "string".upcase
     return "NO, NOT SINCE 1938!"
   end
